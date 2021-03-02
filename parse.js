4d97034a205618f3137bfbac5bc40322dd51090b
mgcioce <mgcioce@gmail.com>
2021-03-01 21:06:15 -0500

    pushing current work

--- parse.js
+++ parse.js
@@ -5,8 +5,8 @@
  * Purpose: adds text parsing and editing functions
  */
 
-function replace(originalString,regex,replaceString) {
-    return originalString.replace(regex,replaceString);
-  }
-  
-  console.log(replace('hello','h','y'));
+export const parser = {
+    replace: (originalString,regex,replaceString) => {
+        return originalString.replace(regex,replaceString);
+    }
+} 
--- sheetsInterface.js
+++ sheetsInterface.js
@@ -1,10 +1,11 @@
-
 /**
  * file: sheetsInterface.js
  * Date: 2/28/21
  * Author: Mike Cioce
  * Purpose: This script provides abstraction methods for reading and writing to a google sheet
  */
+
+import parser from parse.js
 
 var BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
 var FUNCTIONS = {
@@ -15,8 +16,8 @@ var RANGE = '{range}';
 
 function readValues(sheetId,cellRange) {
   var url = BASE_URL + FUNCTIONS['spreadsheets.values.get'];
-  replace(url,SHEET_ID,sheetId);
-  replace(url,RANGE,cellRange);
+  parser.replace(url,SHEET_ID,sheetId);
+  parser.replace(url,RANGE,cellRange);
   fetch(url).then(success => {
     console.log(success.json());
   },failure => {
